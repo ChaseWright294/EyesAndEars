261af01ab067ed50540de91e2a03234006782ca0
ChaseWright294 <chasew454@gmail.com>
2025-02-28 14:07:56 -0600

    Created HomeCard.jsx and the components folder; Updated App.jsx
    HomeCard is a component that will eventually store the links to certain tools on the home page and will navigate the user to the pages for the metronome, tuner, and sheet music reader.

---
+++ frontend/src/components/HomeCard.jsx
@@ -0,0 +1,15 @@
+//import the css and ohter needed things once created
+
+function HomeCard({page}) {
+    return (
+    <div className="home-card">
+        <div className="page-info">
+            <h3>{page.title}
+                <p>{page.description}</p>
+            </h3>
+        </div>
+    </div>
+    )
+}
+
+export default HomeCard
