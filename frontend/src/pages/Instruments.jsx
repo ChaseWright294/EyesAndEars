3331a1a328f02260fcf64fa3da8e50eea64fec50
racheltown <racheltown24@gmail.com>
2025-03-02 19:41:31 -0600

    2. Search bar works

--- frontend/src/pages/Instruments.jsx
+++ frontend/src/pages/Instruments.jsx
@@ -1,9 +1,12 @@
+import SearchBar from "./SearchInstruments"
+
 function Instruments()
 {
     return(
         <div>
             <NavBar />
             <h3>Welcome to the Instruments Page</h3>
+            <SearchBar></SearchBar>
         </div>
     )
 }
