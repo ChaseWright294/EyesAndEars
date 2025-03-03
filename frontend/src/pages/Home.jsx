30481149cde1eee81181be4cd2e3569653af55a1
ChaseWright294 <chasew454@gmail.com>
2025-03-02 18:02:19 -0600

    Added the functionality component to HomeCard
    Added the functionality component to the HomeCard component; Combined the two arrays in Home so that they could more easily be passed to the different components

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,7 @@
 import HomeCard from "../components/HomeCard"
-import FunctionalityCard from "../components/FunctionalityCard"
 
 function Home() {
     const pages = [
-      {
-        title: "Tuner",
-        description: "Tune your instrument"
-      },
-      {
-        title: "Metronome",
-        description: "Practice in rhythm"
-      },
-      {
-        title: "Sheet Music Reader",
-        description: "Get help reading your sheet music"
-      }
-    ]
-
-    const functionalities = [
       {
         title: "Tuner",
         description: "Tune your instrument",
@@ -32,7 +16,6 @@ function Home() {
         title: "Sheet Music Reader",
         description: "Get help reading your sheet music",
         link: "/sheetmusic"
-
       }
     ]
   
@@ -41,20 +24,8 @@ function Home() {
         <HomeCard page={pages[0]}></HomeCard>
         <HomeCard page={pages[1]}></HomeCard>
         <HomeCard page={pages[2]}></HomeCard>
-
-        {functionalities.map((func, index) => (
-          <FunctionalityCard
-            key={index}
-            title={func.title}
-            description={func.description}
-            link={func.link}
-          />
-
-        ))}
       </div>
-
     )
-
   }
   
   export default Home
