36b5c96e50cd5eb962099250ca941758569e3863
linalsl <linalslad00@gmail.com>
2025-03-26 16:44:37 -0500

    updated the css for home-page

--- frontend/src/pages/Instruments.jsx
+++ frontend/src/pages/Instruments.jsx
@@ -1,9 +1,12 @@
 import SearchBar from "./SearchInstruments"
+import NavBar from "../components/NavBar";
 
 function Instruments()
 {
     return(
         <div>
+            
+            <h3>Welcome to the Instruments Page</h3>
             <SearchBar></SearchBar>
         </div>
     )
