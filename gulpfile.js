81baeaaf3c41b7c26796cc5b400f220ae8a4d157
Harriet04 <harriet.mangum@gmail.com>
2025-03-30 15:09:14 -0500

    gulpfile made

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const gulp = require('gulp');
+const requireDir = require('require-dir');
+const browserSync = require('browser-sync').create();
+
+global.browserSync = browserSync;
+
+requireDir('./gulp/tasks', {recurse: false});
+
+gulp.task('default', ['watch']);
