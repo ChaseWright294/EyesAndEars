796b79e4b8709819b142489aeb6e89fae1a1bd84
AzeezatOA <azee.akin@gmail.com>
2025-04-01 12:05:12 -0500

    deleted otp stuff and edited database connection string

--- backend/server.js
+++ backend/server.js
@@ -15,39 +15,10 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
-// Helper function to send OTP
-/*
-const sendOtp = async (email, mobile_number, otp) => {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'your_email@gmail.com',
-            pass: 'your_email_password',
-        },
-    });
-
-    const message = {
-        from: 'your_email@gmail.com',
-        to: email,
-        subject: 'Your OTP Code',
-        text: `Your OTP code is ${otp}`,
-    };
-
-    try {
-        await transporter.sendMail(message);
-        console.log('OTP sent');
-    } catch (error) {
-        console.error('Error sending OTP:', error);
-    }
-};
-*/
-
 // Signup endpoint
 app.post('/api/signup', async (req, res) => {
     const { name, email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
-    //const otp = crypto.randomInt(100000, 999999).toString();
-    //const otpExpiry = new Date(Date.now() + 10 * 60 * 1000); // 10 minutes
 
     try {
         await pool.query(
@@ -55,40 +26,12 @@ app.post('/api/signup', async (req, res) => {
             [name, email, hashedPassword]
         );
 
-        //await sendOtp(email, mobile_number, otp);
         res.status(201).send({ message: 'Signup successful.' });
     } catch (error) {
         console.error("Signup error: ", error)
         res.status(500).send({ error: 'Error during signup' });
     }
 });
-
-// Validate OTP endpoint
-/*
-app.post('/api/validate-otp', async (req, res) => {
-    const { email, mobile_number, otp } = req.body;
-
-    try {
-        const result = await pool.query(
-            'SELECT * FROM users WHERE (email = $1 OR mobile_number = $2) AND otp = $3 AND otp_expiry > NOW()',
-            [email, mobile_number, otp]
-        );
-
-        if (result.rowCount === 0) {
-            return res.status(400).send({ error: 'Invalid or expired OTP' });
-        }
-
-        await pool.query(
-            'UPDATE users SET otp = NULL, otp_expiry = NULL WHERE email = $1 OR mobile_number = $2',
-            [email, mobile_number]
-        );
-
-        res.send({ message: 'OTP validated successfully' });
-    } catch (error) {
-        res.status(500).send({ error: 'Error during OTP validation' });
-    }
-});
-*/
 
 // Login endpoint
 app.post('/api/login', async (req, res) => {
@@ -120,6 +63,10 @@ app.post('/api/login', async (req, res) => {
         res.status(500).send({ error: 'Error during login' });
     }
 });
+
+/*app.post('api/sheetmusic', token,  async (req, res) => {
+
+})*/
 
 app.get('/', (req, res) => {
     res.send('Backend is running in VS Code! Hi there!');
