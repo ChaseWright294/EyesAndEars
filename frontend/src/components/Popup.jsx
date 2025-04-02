47cffdc062f62d383f4b035eb20b41602e66baa4
Harriet04 <harriet.mangum@gmail.com>
2025-04-02 17:09:22 -0500

    Improved the CSS of the popup

--- frontend/src/components/Popup.jsx
+++ frontend/src/components/Popup.jsx
@@ -3,16 +3,14 @@ import { Link } from "react-router-dom";
 
 function Popup(props) {
     return(props.trigger) ? (
-        <div classname="popup" >
-            <div classname="popup-inner">
-                <button classname="close-btn" onClick={() => props.setTrigger(false)}>Close</button>
+        <div className="popup" >
+            <div className="popup-inner">
+                <button className="close-btn" onClick={() => props.setTrigger(false)}>Close</button>
                 
                     <div>
-                        <Link to ="/audio" >
-                            <span>saved audios</span>
-                        </Link>
+                        <Link to ="/audio" className='save-nav'>saved audios</Link>
                         
-                        <Link to ="/sheetmusic">uploaded music</Link>
+                        <Link to ="/sheetmusic" className='save-nav'>uploaded music</Link>
                     </div>
                
             </div>
