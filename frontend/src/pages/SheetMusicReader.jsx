714e24a6a280fde3543333403b1e6ba54fb3319d
AimuaIlogienboh <aimualistik11@gmail.com>
2025-04-03 13:30:47 -0500

    Updated this branch
    added music upload file to this branch

--- frontend/src/pages/SheetMusicReader.jsx
+++ frontend/src/pages/SheetMusicReader.jsx
@@ -1,9 +1,14 @@
+import React from "react";
+import MusicFileUpload from "../components/musicfileupload";
+
 function SheetMusicReader() {
     return(
         <div>
-            <p>This is a page for sheet music reader</p>
-        </div>
+      <h1>Sheet Music Reader</h1>
+      <MusicFileUpload />
+      {/* Other components and logic for SheetMusicReader */}
+    </div>
     )
 }
 
-export default SheetMusicReader
+export default SheetMusicReader;
