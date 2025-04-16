fd830d2622073abd36360aee8d9710903f61ed9f
ChaseWright294 <chasew454@gmail.com>
2025-04-15 23:28:53 -0500

    Added play button (not functioning) and fixed formatting for musicxml to render in one continuous strip rather than stacked
    This commit also changes the backend node_modules back to the windows version

--- frontend/src/components/SheetMusicRenderer.jsx
+++ frontend/src/components/SheetMusicRenderer.jsx
@@ -1,15 +1,20 @@
 import * as vexml from '@stringsync/vexml';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import '../css/SheetMusicRenderer.css'
 
 function SheetMusicRenderer({ musicString }) {
     const rendererRef = useRef(null);
+    
+    const onPlayButtonClick = () => {
+        alert("Boop!");
+    }
 
     useEffect(() => {
         if (rendererRef.current && musicString) {
             rendererRef.current.innerHTML = ''; //clear old score
             try { //error checker, because Windows and Mac have different types of musicxml files it turns out
-                vexml.renderMusicXML(musicString, rendererRef.current); //render new score
+                const score = vexml.renderMusicXML(musicString, rendererRef.current); //render new score
+            
             } catch(error) {
                 rendererRef.current.innerHTML ='<p>Error reading .musicxml file. Please be sure you are using the correct type of .musicxml for your OS.</p>'
             }
@@ -17,8 +22,11 @@ function SheetMusicRenderer({ musicString }) {
     }, [musicString]);
 
     return (
-        <div className='renderer'>
-            <div id="renderer" ref={rendererRef}></div>
+        <div>
+            <button className='play-btn' onClick={onPlayButtonClick}>Play</button>
+            <div className='renderer'>
+                <div ref={rendererRef} style={{width: '1175px', overflowX: 'auto'}}/>
+            </div>
         </div>
     );
 }
