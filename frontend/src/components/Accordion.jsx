8a75b2d81b028eb7172162b346eb37b3a6932e91
Harriet04 <harriet.mangum@gmail.com>
2025-04-22 09:02:58 -0500

    pushing code that doesn't work completely so others can mess with it

--- frontend/src/components/Accordion.jsx
+++ frontend/src/components/Accordion.jsx
@@ -1,11 +1,55 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../css/Accordion.css';
+import { Link } from "react-router-dom";
 
 function Accordion (props){
   const [isActive, setIsActive] = useState(false);
-
+  const [musicpath, setMusicPath] = useState("");
+    
   const [selectedMusic, setSelectedMusic] = useState([]);
+  //const instrument_Id = props.children.i_id_fk;           ?param=${instrument_Id}
+    
+    useEffect(() => {
+        const fetchUserMusic = async () => {
+            try {
+                const token = localStorage.getItem("token");
+                const response = await axios.get(`http://localhost:5001/api/upload`, {headers: { Authorization: `Bearer ${token}`}
+                });
+                setSelectedMusic(response.data);             
+                console.log("Fetched User music: ", response.data);
+            } catch(error){
+                console.error("Error fetching user music: ", error);
+            }
+        };
+        fetchUserMusic();
+        
+    }, []); 
+
+  return (
+    <div className="accordion-item">
+      <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
+        <div>{props.children.i_name}</div>
+        <div>{isActive ? '-' : '+'}</div>
+      </div>
+      {isActive && <div className="accordion-content">
+        {selectedMusic.map((music, index) => (
+        <div className="music-card" key={index}>
+            {setMusicPath(music.m_path)}
+            <Link to ={{pathname: "/sheetmusic", state: {musicpath} }}>
+                {music.m_title}
+            </Link>
+        </div>
+        ))}
+        </div>}
+    </div>
+  );
+};
+
+export default Accordion;
+
+
+/*const [selectedMusic, setSelectedMusic] = useState([]);
     
     useEffect(() => {
         const fetchUserMusic = async () => {
@@ -21,23 +65,4 @@ function Accordion (props){
         };
         fetchUserMusic();
         
-    }, []);  
-
-  return (
-    <div className="accordion-item">
-      <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
-        <div>{props.children.i_name}</div>
-        <div>{isActive ? '-' : '+'}</div>
-      </div>
-      {isActive && <div className="accordion-content">
-        {selectedMusic.map((music, index) => (
-        <div key = {index} className="music-card">
-        {music.m_title}
-        </div>
-        ))}
-        </div>}
-    </div>
-  );
-};
-
-export default Accordion;
+    }, []); */
--- frontend/src/pages/SheetMusicReader.jsx
+++ frontend/src/pages/SheetMusicReader.jsx
@@ -2,15 +2,19 @@ import React, { useState } from "react";
 import MusicFileUpload from "../components/MusicFileUpload";
 import SheetMusicRenderer from "../components/SheetMusicRenderer";
 import NavBar from "../components/NavBar";
+import {useLocation} from "react-router-dom";
 
 function SheetMusicReader() {
-  const [file,setFile] = useState(null); //string loaded from file retrieved from MusicFileUpload
+  const location = useLocation();
+  const { musicpath } = location.state; //accessing the state from the location object 
+
+  const [file,setFile] = useState(musicpath); //string loaded from file retrieved from MusicFileUpload
 
   return(
     <div>
       <NavBar />
       {(file) ? "" : <h1>Sheet Music Reader</h1>} {/*removes the 'Sheet Music Reader' if there is a file being rendered*/}
-      <MusicFileUpload setFile={setFile} />
+      {/*<MusicFileUpload setFile={props.m_filepath} />*/}
        
       {/*display renderer if there is a file, and test if there isn't*/}
       <pre>{file ? <SheetMusicRenderer musicString={file} /> : "No file loaded"}</pre>
