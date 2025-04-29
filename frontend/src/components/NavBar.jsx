3b4048714ee3f763ae69d861d1871c5d866f6228
AmwaMosa <aimualistik11@gmail.com>
2025-04-29 11:49:59 -0500

    Update NavBar.jsx
    made nav bar work with hover speak so it reads nav bar out loud

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,29 +1,32 @@
 import { Link } from "react-router-dom";
 import { FaHome, FaMusic, FaGuitar } from "react-icons/fa";
-import "../css/Navbar.css"; 
-import Home from "../pages/Home";
-
-
+import "../css/Navbar.css";
+import HoverSpeak from "./HoverSpeak"; // Import HoverSpeak
 
 function NavBar() {
-    return (
-        <nav className="navbar">
-            <div className="navbar-left">
-                <Link to ="/Home" className="nav-item">
-                    <FaHome className="nav-icon"/>
-                    <span>Home</span>
-                </Link>
-                <Link to ="/dropdown" className="nav-item">
-                    <FaMusic className="nav-icon"/>
-                    <span>Sheet Music Reader</span>
-                </Link>
-                <Link to ="/instruments" className="nav-item">
-                    <FaGuitar className="nav-icon"/>
-                    <span>Instruments</span>
-                </Link>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        {/* Home Link */}
+        <Link to="/Home" className="nav-item">
+          <FaHome className="nav-icon" />
+          <HoverSpeak text="Home" /> {/* HoverSpeak for Home */}
+        </Link>
+
+        {/* Sheet Music Reader Link */}
+        <Link to="/dropdown" className="nav-item">
+          <FaMusic className="nav-icon" />
+          <HoverSpeak text="Sheet Music Reader" /> {/* HoverSpeak for Sheet Music Reader */}
+        </Link>
+
+        {/* Instruments Link */}
+        <Link to="/instruments" className="nav-item">
+          <FaGuitar className="nav-icon" />
+          <HoverSpeak text="Instruments" /> {/* HoverSpeak for Instruments */}
+        </Link>
+      </div>
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
